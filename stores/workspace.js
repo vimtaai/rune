27f623e5d7bc5e31b3ba0753857575711d2570b9
Marton Visnovitz <visnovitz.marton@gmail.com>
2024-10-11 00:39:25 +0200

    feat: add workspace store

---
+++ stores/workspace.js
@@ -0,0 +1,45 @@
+import { State } from "../lib/state.js";
+import {
+  getFilePath,
+  pickDirectory,
+  pickFileFromDirectory,
+} from "../utilities/file-system.js";
+
+export const workspace = new State({
+  root: null,
+  document: null,
+  stylesheet: null,
+
+  get isOpen() {
+    return this.root !== null;
+  },
+
+  async getDocumentPath() {
+    if (this.document === null) {
+      return [];
+    }
+
+    const fullPath = await getFilePath(this.document, this.root);
+    return fullPath.slice(0, -1);
+  },
+
+  async pickWorkspace() {
+    this.root = await pickDirectory();
+  },
+
+  async pickDocument() {
+    this.document = await pickFileFromDirectory({
+      directory: this.root,
+      description: "Documents",
+      types: [".html"],
+    });
+  },
+
+  async pickStylesheet() {
+    this.stylesheet = await pickFileFromDirectory({
+      directory: this.root,
+      description: "Stylesheets",
+      types: [".css"],
+    });
+  },
+});
